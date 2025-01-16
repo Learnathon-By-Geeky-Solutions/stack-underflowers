723eb6caf69dc69fc2aee173acf91d070f4cf6c0
Pronoy-Deb <pronoydeb98@gmail.com>
2025-01-16 22:38:41 +0600

    added the 11th program

---
+++ Pronoy_Debnath/Jagged_Swaps.cs
@@ -0,0 +1,36 @@
+// https://codeforces.com/problemset/problem/1896/A
+
+using System; using System.Collections; using System.Linq.Expressions;
+
+class Program {
+    static void test(int tc) {
+        var vals = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int n = vals[0];
+        var ar = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        ps(ar[0] == 1);
+        // Console.WriteLine();
+    }
+
+    const int M = 1000000007;
+    static void Main(string[] args) {
+        int tc = 0, tt = 1;
+        tt = int.Parse(Console.ReadLine());
+        while (tc++ < tt) test(tc);
+    }
+    static void pe<T>(IEnumerable<T> array) { Console.WriteLine(string.Join(" ", array)); }
+    static void ps(bool b) { Console.WriteLine(b? "YES" : "NO"); }
+    static void ck<T>(Expression<Func<T>> expression) {
+        var body = (MemberExpression)expression.Body;
+        string variableName = body.Member.Name;
+        T value = expression.Compile().Invoke();
+        Console.Error.Write($"{variableName} = ");
+        if (value is IEnumerable enu && value is not string) {
+            Console.Error.WriteLine($"[{string.Join(", ", enu.Cast<object>())}]");
+        }
+        else if (value is IDictionary dictionary) {
+            foreach (DictionaryEntry ent in dictionary)
+                Console.Error.WriteLine($"{ent.Key}: {ent.Value}");
+        }
+        else Console.Error.WriteLine(value);
+    }
+}
